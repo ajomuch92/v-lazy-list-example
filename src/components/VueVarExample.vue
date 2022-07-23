fe3ebc6a1140b8d6a6dbb58907f088d005a7d340
ajomuch92 <aaronmontes_92@yahoo.es>
2022-07-23 17:50:29 -0600

    refactor: 💡 Adding VueVar package to project as example

---
+++ src/components/VueVarExample.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <vue-var v-for="item in items" :key="item" :data="myMethod(item)">
+      <li slot="default" slot-scope="{data}">
+        Item {{item}} | My Method {{data}} | {{data % 2 == 0 ? 'Pair':'Odd'}}
+      </li>
+    </vue-var>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'VueVarExample',
+  data: () => ({
+    items: [],
+  }),
+  created() {
+    this.items = Array.from({length: 10}, (_, i) => i + 1);
+  },
+  methods: {
+    myMethod(number) {
+      return Math.pow(number, 2);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
